6143ef1aca4037293de6538ff260664bde9c870b
Penapple <kxy156@case.edu>
2016-09-29 11:10:14 -0400

    Create 1.4.py

---
+++ 1.4.py
@@ -0,0 +1,11 @@
+def replace(str):
+    input = list(str)
+    for i in range(0,len(input)):
+        if input[i] == ' ':
+            input[i] = '%20'
+        print(input[i], end ='')
+            
+    
+if __name__ == "__main__":
+    print ("# 1.4")
+    replace('You Know Nothing Mr John Snow')
