f72434427c496ee623a0828c4e7c3d8aa0e4f67e
Penapple <kxy156@case.edu>
2016-09-29 17:41:05 -0400

    Update 1.7.py

--- 1.7.py
+++ 1.7.py
@@ -2,21 +2,25 @@ x = [ [1, 2, 3],
       [4, 5, 6],
       [7, 0, 9],
       [10, 11, 12] ]
-
-          
-def setZerousingnp():  #method2--use numpy
-    import numpy as np  
-    flag = 0   
+         
+def setZero(): 
+    row = {}
+    col = {} 
+    for i in range(0, len(x)):
+        row[i] = False 
+    for j in range(0, len(x[0])):
+        col[j] = False  
     for i in range(0, len(x)):
         for j in range(0, len(x[0])):
             if x[i][j] == 0:
-                flag += 1
-    if flag >= 1:
-        zerom = np.zeros((len(x), len(x[0])))
-    else: 
-        zerom = x
-    print (zerom)
+                row[i] = True
+                col[j] = True
+    for i in range(0, len(x)):
+        for j in range(0, len(x[0])):
+            if row[i] == True or col[j] == True:
+                x[i][j] = 0
+    print (x)
             
 if __name__ == "__main__":
     print ("# 1.7")
-    setZerousingnp()
+    setZero()
