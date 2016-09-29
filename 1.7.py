8b1820fd75ec38d843876329811169c530458bbb
Penapple <kxy156@case.edu>
2016-09-29 17:22:36 -0400

    Create 1.7.py

---
+++ 1.7.py
@@ -0,0 +1,23 @@
+x = [ [1, 2, 3],
+      [4, 5, 6],
+      [7, 0, 9],
+      [10, 11, 12] ]
+
+          
+def setZerousingnp(): 
+    import numpy as np  
+    flag = 0   
+    for i in range(0, len(x)):
+        for j in range(0, len(x[0])):
+            if x[i][j] == 0:
+                flag += 1
+
+    if flag >= 1:
+        zerom = np.zeros((len(x), len(x[0])))
+    else: 
+        zerom = x
+    print (zerom)
+            
+if __name__ == "__main__":
+    print ("# 1.7")
+    setZerousingnp()
