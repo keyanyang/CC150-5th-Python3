85984a89cfd05991016de64895d44a6475b36d3a
Penapple <kxy156@case.edu>
2016-09-29 09:51:02 -0400

    Create 1.2.py

---
+++ 1.2.py
@@ -0,0 +1,13 @@
+def reverse(string):
+    a = string
+    print(a[::-1])
+
+def reverse2(string):
+    a = string
+    for i in range(0,len(a)):
+        print(a[-(i+1)], end = '')
+
+if __name__ == "__main__":
+    print ("# 1.2")
+    print (reverse('pen apple'))
+    print (reverse2('pen apple'))
