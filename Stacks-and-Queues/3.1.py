5962928c60971ac35398111e0e7678dfc62acf9f
Keyan(Kyle) Yang <kxy156@case.edu>
2016-10-03 19:10:25 -0400

    Update 3.1.py

--- Stacks-and-Queues/3.1.py
+++ Stacks-and-Queues/3.1.py
@@ -11,38 +11,44 @@ class arrayStacks:
         return stacknum * self.stacksize + self.pointer[stacknum]
 
     def push(self, stacknum, value):
-        if self.pointer[stacknum] + 1 < self.stacksize:
+        if stacknum >= self.n:
+            return 'stack number is out of space'
+        elif self.pointer[stacknum] + 1 < self.stacksize:
             self.pointer[stacknum] += 1
             self.array[self.stacktop(stacknum)] = value
+            return 'add {} in stack {}'.format(value, stacknum)
         else:
             return 'out of space'
     
     def pop(self, stacknum):
-        if self.pointer[stacknum] < 0:
+        if stacknum >= self.n:
+            return 'stacknumber is out of space'
+        elif self.pointer[stacknum] < 0:
             return 'this stack is empty and cannot be popped'
         else:
             data = self.array[self.stacktop(stacknum)]
             self.array[self.stacktop(stacknum)] = None
             self.pointer[stacknum] -= 1
-            return data
+            return 'remove {} in stack {}'.format(data, stacknum)
         
     def showTop(self, stacknum):
         if self.pointer[stacknum] < 0:
             return 'empty stack'
         else:
-            return self.array[self.stacktop(stacknum)]
+            return '{} is the latest value in stack {}'.format(self.array[self.stacktop(stacknum)], stacknum)
     
     def showArray(self):
         return self.array    
     
 array = arrayStacks(10,3)       #array=[None,...None] where there is 30 Nones
-array.push(2, 20)       #In stack2, the first value is 20
-array.push(2, 30)       #In stack2, the next value is 30
+print (array.push(2, 20))       #In stack2, the first value is 20
+print (array.push(2, 30))       #In stack2, the next value is 30
+print (array.push(4, 30))       #no stack4  
 print (array.pop(1))        #remove and show the latest value of stack 1, which should be empty 
 print (array.pop(2))        #remove and show the latest value of stack 2           
 print (array.showTop(2))        #show the current first value of stack2 
-array.push(2, 10)
+print (array.push(2, 10))
 print (array.showTop(2)) 
-array.push(0, 20)
-array.push(0, 30) 
+print (array.push(0, 20))
+print (array.push(0, 30))
 print (array.showArray())       #show this array which includes three stacks
