f59d66a11f6cc06899073466ce92911097854943
Keyan(Kyle) Yang <kxy156@case.edu>
2016-10-04 12:05:16 -0400

    Update 3.5.py

--- Stacks-and-Queues/3.5.py
+++ Stacks-and-Queues/3.5.py
@@ -5,20 +5,27 @@ class MyQueue:
     
     def push(self, num):
         self.stack1.append(num)
+        l = len(self.stack2) + 1
         del self.stack2[:]
         i = 1
-        while i <= len(self.stack1):
+        if l <= 0:
             self.stack2.append(self.stack1[-i])
-            i += 1
+        else:
+            while i <= l:
+                self.stack2.append(self.stack1[-i])
+                i += 1
     
     def pop(self):
-        return self.stack2.pop()
+        return 'pop {}'.format(self.stack2.pop())
 
 mq = MyQueue()
 print (mq.stack1, mq.stack2)    
 mq.push(1)
+print (mq.stack1, mq.stack2)    
 mq.push(2)
+print (mq.stack1, mq.stack2)    
 mq.push(3)
+print (mq.stack1, mq.stack2)   
 mq.push(4)
 print (mq.stack1, mq.stack2) 
 mq.push(50)
@@ -28,3 +35,7 @@ print (mq.pop())
 print (mq.pop())
 print (mq.pop())
 print (mq.stack1, mq.stack2)    #FIFO
+mq.push(3)
+print (mq.stack1, mq.stack2)
+mq.push(4)
+print (mq.stack1, mq.stack2)
