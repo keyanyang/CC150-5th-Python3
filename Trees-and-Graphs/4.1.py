608a0877e5d21f3f4f94d7acad53731cf05135d7
Keyan(Kyle) Yang <kxy156@case.edu>
2016-10-07 17:19:33 -0400

    Create 4.1.py

---
+++ Trees-and-Graphs/4.1.py
@@ -0,0 +1,33 @@
+class bTree:
+    def __init__(self, value, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def height(node):
+    if node == None:
+        return 0
+    else:
+        return max(height(node.left), height(node.right)) + 1
+    
+def balance(bt):
+    if bt == None:
+        return True
+    else:
+        return abs(height(bt.left) - height(bt.right)) <= 1
+
+BT1 = bTree(1, bTree(2, bTree(4), bTree(5)), bTree(3))    
+print('The height of leftsubtree:')
+print(height(BT1.left))
+print('The height of rightsubtree:')
+print(height(BT1.right))
+print('Is is balenced?')
+print(balance(BT1))
+print('-------------')
+BT2 = bTree(1, bTree(2, bTree(4, bTree(6), bTree(7)), bTree(5)), bTree(3))    
+print('The height of leftsubtree:')
+print(height(BT2.left))
+print('The height of rightsubtree:')
+print(height(BT2.right))
+print('Is is balenced?')
+print(balance(BT2))
