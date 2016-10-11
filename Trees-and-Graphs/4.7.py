2249e11eef82a0622e307cdaa998ad650e4552df
Keyan(Kyle) Yang <kxy156@case.edu>
2016-10-11 16:08:30 -0400

    Update 4.7.py

--- Trees-and-Graphs/4.7.py
+++ Trees-and-Graphs/4.7.py
@@ -15,25 +15,38 @@ def makebt(depth, p = None):
     bt = bTree(random.randrange(0,20))
     bt.parent = p
     bt.left = makebt(depth - 1, bt)
-    bt.right = makebt(depth - 1, bt)
+    bt.right = makebt(depth -1 , bt)
     return bt
-
 
 def common(node1, node2):
     node1.color = "mark"
     while node1.parent != None:
         node1.parent.color = "mark"
-        node1 = node1.parent
-    node1.color = "mark"
+        node1 = node1.parent    
     while node2.parent != None:
-        if node2.color == "mark":
-            return(node2.content)
+        if node2.color == "mark": return node2.content
         node2 = node2.parent
-    if node2.color == "mark":
-        return(node2.content)       
+    if node2.color == "mark": return node2.content    
+    
+def btl(bt):        #just for observing the bt better
+    if bt is None: return None
+    ret = [[bt.content]]
+    queue = [bt]
+    while len(queue) > 0:
+        nq = []
+        for node in queue:
+            if node.left != None:
+                nq.append(node.left)
+            if node.right != None:
+                nq.append(node.right)
+        queue = nq
+        if len(queue) == 0: break
+        ret.append([x.content for x in queue])
+    return ret
 #
 bt1 = makebt(5)
 print(bt1)
+print(btl(bt1))
 node1 = bt1.left.left
 node2 = bt1.right.right
 commonancestor = common(node1, node2)
