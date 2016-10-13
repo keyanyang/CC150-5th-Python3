87ff9180a7266ce269875a29457fb8a68216e94b
Keyan(Kyle) Yang <kxy156@case.edu>
2016-10-13 17:37:36 -0400

    Update 9.1.py

--- Recursion-and-Dynamic-Programming/9.1.py
+++ Recursion-and-Dynamic-Programming/9.1.py
@@ -1,12 +1,12 @@
-def stair(n, counter = [0]):
+def stair(n, counter = [0]):         #counter = [0] here will not affect the increase of counter in the recursion
     if n == 0:
         counter[0] += 1
     if n < 0 :
         return 
     else:
-        stair(n-1)
-        stair(n-2)
-        stair(n-3)
+        stair(n-1, counter)
+        stair(n-2, counter)
+        stair(n-3, counter)
     return counter[0]
 
 n = 4
